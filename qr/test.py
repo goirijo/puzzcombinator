6fd157f553cc90fa1dc7720c6df7c52f4e285cbb
John Goiri <jg.goiri@gmail.com>
2016-10-06 21:30:25 -0700

    Can read QR codes!

--- qr/test.py
+++ qr/test.py
@@ -1,9 +1,52 @@
 import cv2
 import detect
 import numpy as np
-from detect import nothing
-import Image
-import qrtools
+import zbar
+
+def convert_to_zbar_image(image):
+    """Create a zbar compatible Image object
+    from a numpy array of pixels
+
+    :image: ndarray
+    :returns: zbar.Image
+
+    """
+    height=image.shape[0]
+    width=image.shape[1]
+    zimage=zbar.Image(width, height, "Y800", np.ndarray.tobytes(ex))
+
+    return zimage
+
+def qr_string_from_zimage(zimage):
+    """Extract the encoded string found inside
+    a given zbar Image
+
+    :zimage: zbar.Image
+    :returns: str or None
+
+    """
+    scanner=zbar.ImageScanner()
+    result=scanner.scan(zimage)
+
+    if result==0:
+        return None
+    else:
+        for symbol in zimage:
+            pass
+        return symbol.data.decode(u"utf-8")
+
+def qr_string_from_numpy(npimage):
+    """Given the pixels in the form of a numpy array,
+    do all the annoying conversions to zbar and
+    get the string from it.
+
+    :npimage: ndarray
+    :returns: str or None
+
+    """
+    return qr_string_from_zimage(convert_to_zbar_image(npimage))
+
+#--------------------------------------------------------------------------------#
 
 cap=cv2.VideoCapture(0)
 
@@ -22,18 +65,21 @@ while(k!=27):
     cv2.imshow("high",high)
 
 
-    expil=Image.fromarray(ex)
-    code=qrtools.QR(expil)
-    if code.decode():
-        print code.data
-        print code.data_type
-        print code.data_to_string()
 
     k=cv2.waitKey(10)
 
-    if k==ord("p"):
-        detect._outer_triangle(detect._big_corners(corners))
+    if k==ord("t"):
+        result=qr_string_from_numpy(ex)
+        if ex is None:
+            print "Could not read"
+        else:
+            print result
 
+    if k==ord("q"):
+        break
+
+    #if k==ord("p"):
+    #    detect._outer_triangle(detect._big_corners(corners))
 
     if k==ord("c"):
         filename="code.jpg"
